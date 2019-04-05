16aa302bd5ca18c9e708fcb1b898f3ede3880b58
Harald Fernengel <harry@kdevelop.org>
2019-04-05 10:55:28 +0200

    Add project owners

--- lesson22-final-project-suggestions/main.js
+++ lesson22-final-project-suggestions/main.js
@@ -3,6 +3,7 @@ let projectsElement = document.getElementById("projects");
 let projects = [
     {
         name: "CalIdea",
+        owner: "Helga",
         description: [
             "Show a calendar with different time zones, making it easier for global teams to schedule events and tasks",
             "Show national holidays",
@@ -21,6 +22,7 @@ let projects = [
     },
     {
         name: "To Eat or Not to Eat",
+        owner: "Katja",
         description: [
             "Plant identification API",
             "Can I eat that?",
@@ -30,6 +32,7 @@ let projects = [
     },
     {
         name: "To Learn List",
+        owner: "Toto",
         description: [
             "List of things you want to learn",
             "Connect with other learners",
@@ -40,6 +43,7 @@ let projects = [
     },
     {
         name: "Desert Tech Model",
+        owner: "John",
         description: [
             "Map around mediterranean sea (Leaflet)",
             "Clickable points on the map for solar panel positions",
@@ -49,6 +53,7 @@ let projects = [
     },
     {
         name: "Best Dictionary Ever",
+        owner: "Toto",
         description: [
             "Translation preferences from multiple sources",
             "Gendered words with indicator"
@@ -65,6 +70,7 @@ let projects = [
     },
     {
         name: "Band Aid",
+        owner: "Katja",
         description: [
             "Connect bands / artists with venues",
             "Add profiles for each venue",
@@ -73,6 +79,7 @@ let projects = [
     },
     {
         name: "Sky Map",
+        owner: "Yegi",
         description: [
             "Description about planets in the universe",
             "Planetarium view with path showing information",
@@ -85,6 +92,7 @@ let i = 1;
 
 for (let project of projects) {
     projectsElement.innerHTML += `<h2>${i++}: ${project.name}</h2>`;
+    projectsElement.innerHTML += `Owner: ${project.owner}`;
     let ulElement = document.createElement("ul");
     for (let sentence of project.description) {
         let liElement = document.createElement("li");
